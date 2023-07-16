2b7a0dc321b0c45a79096297dba52cbf117d5307
Flokki217 <flokki217@gmail.com>
2023-07-16 22:47:48 +0200

    qwretryt

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const imageList = galleryItems
+  .map(image => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${image.original}">
+      <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+   </a>
+</li>`;
+  })
+  .join('');
+
+galleryRef.insertAdjacentHTML('afterbegin', imageList);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const FEEDBACK_KEY = 'feedback-form-state';
+const formEl = document.querySelector('.feedback-form');
+
+const {
+  elements: { email, message },
+} = formEl;
+
+const dataObj = {};
+
+formEl.addEventListener('input', throttle(inputHandler, 500));
+
+function inputHandler(evt) {
+  dataObj.email = email.value;
+  dataObj.message = message.value;
+
+  localStorage.setItem(FEEDBACK_KEY, JSON.stringify(dataObj));
+}
+
+let feedbackFormState = JSON.parse(
+  localStorage.getItem('feedback-form-state')
+) || { email: '', message: '' };
+
+email.value = feedbackFormState.email;
+message.value = feedbackFormState.message;
+
+formEl.addEventListener('submit', submitHandler);
+
+function submitHandler(evt) {
+  evt.preventDefault();
+
+  if (email.value === '' || message.value === '') {
+    return alert('All field have be full');
+  }
+
+  console.log(JSON.parse(localStorage.getItem(FEEDBACK_KEY)));
+
+  localStorage.removeItem(FEEDBACK_KEY);
+
+  evt.currentTarget.reset();
+}
